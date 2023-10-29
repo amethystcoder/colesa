0dc2d55c22990193b2a966b819f970e4d2dbd648
amethystcoder <amethystguitaristj@gmail.com>
2023-10-29 20:09:11 +0000

    created methods for sending appointment details

---
+++ new_public/create_appointment.js
@@ -0,0 +1,15 @@
+
+
+function submitform(){
+    let Salutation = document.querySelector(".QR-QID12")
+    let LegalName = document.querySelector("#QR~QID2~1")
+    let LegalSurname = document.querySelector("#QR~QID2~2")
+    let Birthdate = document.querySelector("#QR~QID3")
+    let Requestername = document.querySelector("#QR~QID4")
+    let Sex = document.querySelector("QR~QID5")//Change to name. type is radio
+    let Mobileno = document.querySelector("#QR~QID6")
+    let Email = document.querySelector("#QR~QID7")
+    let AppointmentType = document.querySelector()//QR~QID8~1 Check how to deal with checkboxes online
+    let AppointmentReason = document.querySelector("#QR~QID9")
+    let FollowupMethod = document.querySelector("QR~QID10")//change to name. type is radio
+}
